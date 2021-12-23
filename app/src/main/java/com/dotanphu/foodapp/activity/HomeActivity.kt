17c650f3dfa07bfc9d069d7dcd40954e7fa28451
dotanphu <phudo@tdt.asia>
2021-12-23 16:19:58 +0700

    FIX HINH ANH

--- app/src/main/java/com/dotanphu/foodapp/activity/HomeActivity.kt
+++ app/src/main/java/com/dotanphu/foodapp/activity/HomeActivity.kt
@@ -3,25 +3,34 @@ package com.dotanphu.foodapp.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dotanphu.foodapp.adapter.FoodAdapter
 import com.dotanphu.foodapp.databinding.ActivityHomeBinding
 import com.dotanphu.foodapp.model.Food
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var adapter: FoodAdapter
     private lateinit var binding: ActivityHomeBinding
+
     private var food = arrayListOf<Food>(
-        Food("Hamburger", "hamburger", 8.7),
-        Food("FastFood", "fastfood", 8.7)
-    )
+            Food("Chicken", "https://cdn-icons.flaticon.com/png/512/783/premium/783054.png?token=exp=1640251184~hmac=d4188b81db862ffdcece016bf68abf6a", 8.6),
+            Food("FastFood", "https://cdn-icons-png.flaticon.com/512/2922/2922037.png", 9.6),
+            Food("Noodles", "https://cdn-icons-png.flaticon.com/512/2718/2718224.png", 5.6),
+            Food("Rice", "https://cdn-icons.flaticon.com/png/512/3183/premium/3183463.png?token=exp=1640251184~hmac=b7cd182d6056de286959fed3d58afc19", 7.6),
+            Food("Hamburger", "https://cdn-icons.flaticon.com/png/512/566/premium/566651.png?token=exp=1640251184~hmac=c327d8f879b7599eb939a909e47db5c0", 6.8),
+            Food("French fries", "https://cdn-icons.flaticon.com/png/512/1744/premium/1744944.png?token=exp=1640251184~hmac=787a4ee91019e65b25d898e45a85dbab", 6.6),
+            Food("Hotdog", "https://cdn-icons.flaticon.com/png/512/1789/premium/1789897.png?token=exp=1640251184~hmac=01ad9b4ed4a98b5fbc8027059bc13f3e", 3.3),
+            Food("Drink", "https://cdn-icons.flaticon.com/png/512/2769/premium/2769611.png?token=exp=1640251897~hmac=c708d473d186684e1fb523641f4a3721", 5.8),
+            )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        adapter = FoodAdapter(food)
+        adapter = FoodAdapter(food, this)
         binding.rvPopular.adapter = adapter
         binding.rvPopular.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+                LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
     }
 }
--- app/src/main/java/com/dotanphu/foodapp/adapter/FoodAdapter.kt
+++ app/src/main/java/com/dotanphu/foodapp/adapter/FoodAdapter.kt
@@ -1,36 +1,40 @@
-package com.dotanphu.foodapp.activity
+package com.dotanphu.foodapp.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.dotanphu.foodapp.databinding.ItemListFoodBinding
 import com.dotanphu.foodapp.model.Food
 
-class FoodAdapter(private var listFood: List<Food>) :
+class FoodAdapter(
+        private var listFood: List<Food>,
+        private var context: Context) :
     RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             ItemListFoodBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
-            )
+            ), context
         )
     }
 
-    override fun onBindViewHolder(holder: FoodAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(listFood[position])
     }
 
     override fun getItemCount() = listFood.size
 
-    class ViewHolder(private var binding: ItemListFoodBinding) :
+    class ViewHolder(private var binding: ItemListFoodBinding, private var context: Context) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(food: Food) {
             binding.tvTittle.text = food.tittle
+            Glide.with(context).load(food.pic).into(binding.imgPic)
             binding.tvPrice.text = food.price.toString()
         }
-
     }
 }
